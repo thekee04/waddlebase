a3e7434ab154ca789e85271ec1876bc5ca9e2905
BoingK <83502726+thekee04@users.noreply.github.com>
2021-10-05 09:24:12 +0100

    Basic database with json

---
+++ db.py
@@ -0,0 +1,21 @@
+import json
+
+#Dictionary commands
+
+DB_FILE = ".dictionary.json"
+
+def read():
+    with open(DB_FILE, "r") as f:
+        data = f.read()
+    try:
+        return json.loads(data)
+    except:
+        print("Corrupted database file.")
+
+def update(data):
+    with open(DB_FILE, "w+") as f:
+        f.write(json.dumps(data))
+
+def sort(data):
+    #return {k: v for k, v in sorted(data.items(), key=lambda item: -item[1]["ELO"])}
+    return
